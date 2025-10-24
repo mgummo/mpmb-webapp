f0310525d9ced57c5727d529e8dd9ed239bac8b5
Mark Gummo <mark.gummo@gmail.com>
2025-10-24 00:09:29 -0400

    string-utils.js file

---
+++ string-util.js
@@ -0,0 +1,10 @@
+function replace_and_report(str, pattern, replacement) {
+    let count = 0;
+    const replaced = str.replace(pattern, (...args) => {
+        count++;
+        return typeof replacement === "function"
+            ? replacement(...args)
+            : replacement;
+    });
+    return [replaced, count];
+}
